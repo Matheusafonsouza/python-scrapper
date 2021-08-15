e53699a5290373ee1f166a5dd12c6479c0ca3d5f
matheusafonsouza <matheusafonsouza@gmail.com>
2021-08-15 14:21:46 -0300

    getting new from g1 site

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('hello world!')
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://g1.globo.com/')
+
+content = response.content
+
+website = BeautifulSoup(content, 'html.parser')
+
+new = website.find('div', attrs={'class': 'feed-post-body'})
+
+print(new.prettify())
