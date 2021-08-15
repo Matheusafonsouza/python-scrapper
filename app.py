1c56270b046c67d0a925379fed0e96933ccc8abd
matheusafonsouza <matheusafonsouza@gmail.com>
2021-08-15 14:55:29 -0300

    building csv file

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+news_list = []
 
 response = requests.get('https://g1.globo.com/')
 
@@ -7,9 +10,16 @@ content = response.content
 
 website = BeautifulSoup(content, 'html.parser')
 
-new = website.find('div', attrs={'class': 'feed-post-body'})
-title = new.find('a', attrs={'class': 'feed-post-link'})
-subtitle = new.find('div', attrs={'class': 'feed-post-body-resumo'})
+news = website.findAll('div', attrs={'class': 'feed-post-body'})
 
-print(title.text)
-print(subtitle.text)
+for new in news:
+    title = new.find('a', attrs={'class': 'feed-post-link'})
+    subtitle = new.find('div', attrs={'class': 'feed-post-body-resumo'})
+
+    if subtitle:
+        news_list.append([title.text, subtitle.text, title['href']])
+    else:
+        news_list.append([title.text, '', title['href']])
+
+news = pd.DataFrame(news_list, columns=['Title', 'Subtitle', 'Link'])
+news.to_csv('news.csv', index=False)
