75e66be3a7479b03231d37724ec9951ab1511a03
matheusafonsouza <matheusafonsouza@gmail.com>
2021-08-22 16:27:03 -0300

    selenium on airbnb

--- selenium/app.py
+++ selenium/app.py
@@ -1,7 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
-driver.get('https://www.google.com.br/')
+options = Options()
+options.add_argument('window-size=400,800')
+# options.add_argument('--headless')
 
-driver_input = driver.find_element_by_css_selector("[title^='Pesquisar']")
-driver_input.send_keys('doctor strange')
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.airbnb.com/')
+
+sleep(10)
+
+driver_input = driver.find_element_by_tag_name('input')
+driver_input.send_keys('São paulo')
+driver_input.submit()
+
+sleep(3)
+driver.find_element_by_css_selector('button > img').click()
+sleep(3)
+driver.find_element_by_xpath("//button[text()='Pular']").click()
+sleep(3)
+driver.find_element_by_xpath("//button[text()='Pular']").click()
+
+web_content = driver.page_source
+
+sleep(3)
+
+web = BeautifulSoup(web_content, 'html.parser')
+print(web.prettify())
